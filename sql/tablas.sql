2d72a77d7cd42e44e8eba40384fc327aa911df5d
Antonio Reyes <antonioreyesrabel.guadalupe@alumnado.fundacionloyola.net>
2024-02-22 08:55:41 +0100

    asdf

--- sql/tablas.sql
+++ sql/tablas.sql
@@ -1,23 +1,22 @@
-
 -- Estructura de tabla para la tabla `clase`
-CREATE TABLE `clase` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `clase` (
+  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `nombreClase` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Estructura de tabla para la tabla `palabras`
-CREATE TABLE `palabras` (
-  `idPalabra` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `palabras` (
+  `idPalabra` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `audio` blob DEFAULT NULL,
   `idClase` int(11) DEFAULT NULL,
-  `palabra` varchar(255) DEFAULT NULL
+  `palabra` varchar(255) DEFAULT NULL,
+  FOREIGN KEY (`idClase`) REFERENCES `clase` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Estructura de tabla para la tabla `traducciones`
-CREATE TABLE `traducciones` (
-  `idTraduccion` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `traducciones` (
+  `idTraduccion` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `significados` text DEFAULT NULL,
-  `idPalabra` int(11) DEFAULT NULL
+  `idPalabra` int(11) DEFAULT NULL,
+  FOREIGN KEY (`idPalabra`) REFERENCES `palabras` (`idPalabra`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-
