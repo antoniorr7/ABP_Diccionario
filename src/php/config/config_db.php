60378d02cd11712eb20a1879ef591c625df8a9da
Antonio Reyes <antonioreyesrabel.guadalupe@alumnado.fundacionloyola.net>
2024-02-22 08:55:26 +0100

    sql bien y hosting

--- src/php/config/config_db.php
+++ src/php/config/config_db.php
@@ -1,7 +1,12 @@
 <?php
 
- define('HOST', 'localhost');   // Dirección del servidor MySQL
- define('USER', 'root');    // Nombre de usuario de MySQL
- define('PASSWORD', ''); // Contraseña de MySQL
- define('DATABASE', 'diccionario'); // Nombre de la base de datos
+//  define('HOST', 'localhost');   // Dirección del servidor MySQL
+//  define('USER', 'root');    // Nombre de usuario de MySQL
+//  define('PASSWORD', ''); // Contraseña de MySQL
+//  define('DATABASE', 'diccionario'); // Nombre de la base de datos
+ 
+ define('HOST', '2daw.esvirgua.com');   // Dirección del servidor MySQL
+ define('USER', 'user2daw_16');    // Nombre de usuario de MySQL
+ define('PASSWORD', 'CuW^nSP1*^$('); // Contraseña de MySQL
+ define('DATABASE', 'user2daw_BD2-16'); // Nombre de la base de datos
  
